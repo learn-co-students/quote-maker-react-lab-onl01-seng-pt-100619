41684fc1b5671081beb9ccdca6b5b000ef81a47e
LizLaffitte <liz.kossakowski@gmail.com>
2020-07-30 17:02:05 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,21 @@
 export default (state = [], action) => {
-  return state;
+  switch(action.type){
+    case 'ADD_QUOTE':
+      return state.concat(action.quote)
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId)
+    case 'UPVOTE_QUOTE':
+      const upIndex = state.findIndex(quote => quote.id === action.quoteId)
+      const upQuote = state[upIndex]
+      upQuote.votes += 1
+      return [...state.slice(0, upIndex), upQuote, ...state.slice(upIndex + 1)]
+    case 'DOWNVOTE_QUOTE': 
+      const index = state.findIndex(quote => quote.id === action.quoteId)
+      const quote = state[index]
+      quote.votes > 0 ? quote.votes -= 1 : quote.votes = 0
+      return [...state.slice(0, index), quote, ...state.slice(index + 1)]
+    default:
+      return state
+  }
 }
+
